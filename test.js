3fd281d6ca45b62abfe951c39af068807818a8e5
Jacob Blakely <jacob@codekirei.com>
2015-10-26 16:19:45 -0700

    write tests

--- test.js
+++ test.js
@@ -6,6 +6,7 @@
 // NPM
 const assert = require('chai').assert
 const sinon = require('sinon')
+const clone = require('lodash.clonedeep')
 
 // Local
 const ti = require('./')
@@ -13,28 +14,91 @@ const ti = require('./')
 //----------------------------------------------------------
 // Tests
 //----------------------------------------------------------
+const types = {
+  'undefined': undefined,
+  'null': null,
+  'string': 'abc',
+  'boolean': true,
+  'number': 1,
+  'function': () => {},
+  'array': [],
+  'object': {}
+}
+
 describe('iterate', () => {
   it('call cb with (val, key) params for each pair in obj', () => {
     const testObj = {'foo': 'bar', 'baz': 'qux'}
     const spy = sinon.spy()
     ti.iterate(testObj, spy)
-    assert(spy.calledTwice)
-    assert(spy.withArgs('bar', 'foo').calledOnce)
-    assert(spy.withArgs('qux', 'baz').calledOnce)
+    assert(spy.calledTwice, 'call cb twice')
+    assert(spy.withArgs('bar', 'foo').calledOnce, 'call cb with (bar, foo)')
+    assert(spy.withArgs('qux', 'baz').calledOnce, 'call cb with (qux, baz)')
   })
 })
 
 describe('allTypes', () => {
-  it('if cb, return iterator func')
-  it('else return types obj')
+  it('if cb, iterate', () => {
+    const spy = sinon.spy()
+    const callCount = Object.keys(types).length
+    ti(spy)
+    assert(spy.called, 'call cb')
+    assert.equal(callCount, spy.callCount, 'call cb for each type')
+  })
+
+  it('else return types obj', () => {
+    const testObj = ti()
+    assert.deepEqual(Object.keys(types), Object.keys(testObj))
+    Object.keys(testObj).map(key => {
+      key === 'function'
+        ? assert.equal(types.function.toString(), testObj.function.toString())
+        : assert.deepEqual(types[key], testObj[key])
+    })
+  })
 })
+
 describe('exclude', () => {
-  it('remove all specified types')
-  it('if cb, return iterator func')
-  it('else return modified types obj')
+  const exclusions = ['function', 'null']
+
+  it('if cb, return iterator func', () => {
+    const spy = sinon.spy()
+    const callCount = Object.keys(types).length - exclusions.length
+    ti.exclude(exclusions, spy)
+    assert(spy.called, 'call cb')
+    assert.equal(callCount, spy.callCount, 'call cb for each type')
+  })
+
+  it('else return modified types obj', () => {
+    const typesClone = clone(types)
+    exclusions.map(type => delete typesClone[type])
+    assert.deepEqual(typesClone, ti.exclude(exclusions))
+  })
+
+  it('handle single string param', () => {
+    const typesClone = clone(types)
+    const testType = 'function'
+    delete typesClone[testType]
+    assert.deepEqual(typesClone, ti.exclude(testType))
+  })
 })
+
 describe('include', () => {
-  it('include only specified types')
-  it('if cb, return iterator func')
-  it('else return modified types obj')
+  const inclusions = ['array', 'object']
+
+  it('if cb, return iterator func', () => {
+    const spy = sinon.spy()
+    const callCount = inclusions.length
+    ti.include(inclusions, spy)
+    assert(spy.called, 'call cb')
+    assert.equal(callCount, spy.callCount, 'call cb for each type')
+  })
+
+  it('else return modified types obj', () => {
+    const expected = {'array': [], 'object': {}}
+    assert.deepEqual(expected, ti.include(inclusions))
+  })
+
+  it('handle single string param', () => {
+    const expected = {'array': []}
+    assert.deepEqual(expected, ti.include('array'))
+  })
 })
