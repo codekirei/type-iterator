e4fb949f4dae1b7f8173914410f0eb64940e7ef5
Jacob Blakely <jacob@codekirei.com>
2015-10-26 15:31:22 -0700

    write test for iterate function

--- test.js
+++ test.js
@@ -5,6 +5,7 @@
 //----------------------------------------------------------
 // NPM
 const assert = require('chai').assert
+const sinon = require('sinon')
 
 // Local
 const ti = require('./')
@@ -13,8 +14,16 @@ const ti = require('./')
 // Tests
 //----------------------------------------------------------
 describe('iterate', () => {
-  it('call cb with (val, key) params for each pair in obj')
+  it('call cb with (val, key) params for each pair in obj', () => {
+    const testObj = {'foo': 'bar', 'baz': 'qux'}
+    const spy = sinon.spy()
+    ti.iterate(testObj, spy)
+    assert(spy.calledTwice)
+    assert(spy.withArgs('bar', 'foo').calledOnce)
+    assert(spy.withArgs('qux', 'baz').calledOnce)
+  })
 })
+
 describe('allTypes', () => {
   it('if cb, return iterator func')
   it('else return types obj')
